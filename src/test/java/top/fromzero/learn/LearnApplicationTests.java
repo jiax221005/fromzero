36fed10320332009229c072be926092c46b5a369
JiaxJapan <hellojasonlord@gmail.com>
2022-10-05 22:25:41 +0900

    initial_1

---
+++ src/test/java/top/fromzero/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fromzero.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
